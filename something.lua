f2ea59ae1223269203980816b9d7e02edb2027e7
hillsjpg <mighty2334@gmail.com>
2025-03-11 11:42:12 +0200

    Update something.lua

--- something.lua
+++ something.lua
@@ -1,6 +1,6 @@
 shared["F​F​l​a​g​A​X​C​o​m​b​i​n​e​G​e​t​O​u​t​f​i​t​D​i​s​p​a​t​c​h​e​s​I​X​P​2"] = {
     ["Main"] = {
-        ['Loader'] = { ['YQLLN6OVplhexPNacUujgMixxMvikyb9'] = ('') },
+        ['Loader'] = { ['KEY'] = ('YQLLN6OVplhexPNacUujgMixxMvikyb9') },
     },
     
     ["Universal"] = {
